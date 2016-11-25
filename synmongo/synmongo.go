2da56ee8eeb588904eee8f67f216b7f8a79e024a
Mani Soundararajan <mani.sound@gmail.com>
2016-11-26 04:45:59 +0530

    Added local packages

---
+++ synmongo/synmongo.go
@@ -0,0 +1,18 @@
+package synmongo
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/mgo.v2"
+)
+
+func GetMongo() *mgo.Session {
+	mongoServer := os.Getenv("SYNPOST_MONGO_SERVER")
+	session, err := mgo.Dial(mongoServer)
+	if err != nil {
+		log.Panic("Unrecoverable error in getMongo: ", err)
+	}
+
+	return session
+}
