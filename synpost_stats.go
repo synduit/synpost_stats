a180572aa3822443dba4656d71a3f0a5e69768d4
Mani Soundararajan <mani.sound@gmail.com>
2016-11-26 21:29:59 +0530

    Reporting broken scheduled AR

--- synpost_stats.go
+++ synpost_stats.go
@@ -39,13 +39,19 @@ func reportStats() {
 	defer c.Close()
 
 	var ch = make(chan error)
-	var err error
+	var err1 error
+	var err2 error
 	for {
 		go reportPendingImportJobs(session, c, ch)
-		err = <-ch
+		go reportBrokenScheduledAutoresponders(session, c, ch)
+		err1 = <-ch
+		err2 = <-ch
 		c.Flush()
-		if err != nil {
-			panic(err)
+		if err1 != nil {
+			panic(err1)
+		}
+		if err2 != nil {
+			panic(err2)
 		}
 		time.Sleep(time.Second * 10)
 	}
@@ -58,8 +64,21 @@ func reportPendingImportJobs(session *mgo.Session, c *statsd.Client, ch chan err
 		log.Print("Unrecoverable error in reportPendingImportJobs: ", err)
 		ch <- err
 	} else {
-		log.Printf("Number of import jobs: %d", n)
+		log.Printf("Number of pending import jobs: %d", n)
 		c.Gauge("jobs.import_pending", n)
+		ch <- nil
+	}
+}
+
+func reportBrokenScheduledAutoresponders(session *mgo.Session, c *statsd.Client, ch chan error) {
+	coll := session.DB("synpost").C("Campaign")
+	n, err := coll.Find(bson.M{"type": "scheduled-autoresponder", "segment": bson.M{"$exists": false}}).Count()
+	if err != nil {
+		log.Print("Unrecoverable error in reportBrokenScheduledAutoresponders: ", err)
+		ch <- err
+	} else {
+		log.Printf("Number of broken scheduled autoresponders: %d", n)
+		c.Gauge("campaigns.scheduled_ar.broken", n)
 		ch <- nil
 	}
 }
